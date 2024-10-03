bb99722650601766e1cdbec6aeb5aa0be828c7fa
GustavoFantin <gustavofantinbarros@gmail.com>
2024-10-03 11:10:24 -0700

    Lodash doubleing array

--- ex01.js
+++ ex01.js
@@ -3,7 +3,9 @@
 const _ = require('lodash');
 
 const numbers = [1, 2, 3, 4, 5];
-
-// your code here
+function double (n) {
+    return n * 2;
+}
+let doubled = _.map(numbers, double)
 
 console.log(doubled); // Expected output: [2, 4, 6, 8, 10]
